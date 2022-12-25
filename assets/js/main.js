fc9211a8db3d654718b7dd60af4071414b1654dc
Lautaro-Veron <121395937+Lautaro-Veron@users.noreply.github.com>
2022-12-25 20:57:07 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,9 @@
 /*=============== Contact Form =============== */
 function SendMail () {
     var params = {
-        from_name : document.getElementById("contact-name").value,
-        email_id : document.getElementById("contact-email").value,
-        message : document.getElementById("contact-message").value
+        from_name : document.getElementById("#contact-name").value,
+        email_id : document.getElementById("#contact-email").value,
+        message : document.getElementById("#message").value
     }
     emailjs.send("service_xqiy6gy", "template_dua0ysq", params).then(function(res) {
         alert("Success!" + res.status);

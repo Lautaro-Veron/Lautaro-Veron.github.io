ad39c339d70ffdc96ebccbbc8fd63474ea35d6f5
Lautaro-Veron <121395937+Lautaro-Veron@users.noreply.github.com>
2022-12-25 13:58:24 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*=============== Active Link =============== */
+
+/*=============== Mixitup Filter =============== */
+
+/*=============== Testimonials Swiper =============== */
+
+/*=============== Contact Form =============== */
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+Message = document.getElementById('message'),
+contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    
+    //check if the field has a value
+    
+    if (contactName.value === '' || contactEmail.value === '' || Message.value === '') {  //add and remove color
+        contactMessage.classList.remove('color-light');
+        contactMessage.classList.add('color-dark');
+
+        //show message
+        contactMessage.textContent = 'Write all the input fields';
+    } else{
+        // serviceID - templeteID - #form - publickey
+        emailjs.sendForm('service_xqiy6gy','template_dua0ysq','#contact-form','L4iHUbaGswW1j_qEl');
+
+        .then(() => {
+            //show message and add color + dot to open emoji
+            contactMessage.classList.add('color-light');
+            contactMessage.textContent = 'Message sent ✔';
+
+            //remove message after 5 seconds
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000);
+        });
+    }
+};
+
+contactForm.addEventListener('submit', sendEmail);
+
